bf62db3f3401336b26b13fba435524c413668fdf
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-03-24 00:30:13 +0800

    Add files via upload

---
+++ script/card_counter_constants_ext.lua
@@ -0,0 +1,35 @@
+--Cards with double names
+
+--Commonly used cards
+CARD_CONSTELLARKNIGHT_TROIVERNUM        = 2002000086
+CARD_TOON_WORLD							= 15259703
+CARD_NATURIA_WENDI						= 2002000062
+CARD_NEKROZ_OUROBOROS					= 2002000080
+CARD_THE_AZURE_PROJECT             = 2003000037
+
+--Commonly used Tokens
+
+--Specific Special Summons
+
+--Commonly used Rush Cards
+
+--Commonly used counters
+COUNTER_BLAZE						= 0x271
+COUNTER_HONEY						= 0x272
+COUNTER_SPARKWAVE       = 0x2a7
+
+--Archetypes
+SET_EXODIA_NECROSS 				= 0x10de
+SET_NUMBER_ALTERNATIVE 				= 0x9048
+SET_INJECTION_FAIRY 			= 0x990
+SET_SHINOBIRD 					= 0x991
+SET_GUARDRAGON 					= 0x992
+--SET_METALMORPH 					= 0x993
+SET_W_NEBULA					   = 0x994
+SET_PIKERU						 = 0x995
+SET_CURRAN						 = 0x996
+SET_STELLARNOVA 				   = 0x997
+SET_AZURIST                        = 0xf16
+SET_STARRYTAIL                     = 0xf13
+SET_LYCANSQUAD                     = 0xf17
+SET_SPARKWAVE                      = 0x2a7
