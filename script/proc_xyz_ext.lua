22d91a61bfc1d52fb957f9bcaa007f74c3bf455f
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-10-18 18:18:45 +0800

    more fixes

--- script/proc_xyz_ext.lua
+++ script/proc_xyz_ext.lua
@@ -1,3 +1,4 @@
+Xyz.InfiniteMats=98
 function Xyz.MatFilter2(c,f,lv,xyz,tp)
     if f and not f(c,xyz,SUMMON_TYPE_XYZ|MATERIAL_XYZ,tp) then return false end
     if not c:HasLevel() and not c:IsHasEffect(EFFECT_XYZ_LEVEL) then return fasle end
