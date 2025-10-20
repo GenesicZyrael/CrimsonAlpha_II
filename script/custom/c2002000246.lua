f926ca59bf00210810e30914d1866a535723a4a7
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-10-20 11:25:30 +0800

    Update c2002000246.lua

--- script/custom/c2002000246.lua
+++ script/custom/c2002000246.lua
@@ -11,6 +11,10 @@ function s.initial_effect(c)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,tp)
+	return c:IsMonster() and c:IsReleasable()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroupCost(tp,nil,1,true,nil,nil) end
