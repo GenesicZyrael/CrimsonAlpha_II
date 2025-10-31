aa85708fb7a5a0955cc1d010fe902474ef1d92b4
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-11-01 02:48:31 +0800

    Bug Fix
    * Shaddollswarm Ouroboros was only adding back Spells and Traps

--- script/custom/c2002000251.lua
+++ script/custom/c2002000251.lua
@@ -118,7 +118,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.thfilter(c)
-	return c:IsSetCard(SET_SHADDOLL) and c:IsSpellTrap() and c:IsAbleToHand()
+	return c:IsSetCard(SET_SHADDOLL) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
