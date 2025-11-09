b43f909bea1b5014cf62d6c17a3c6f4a8e720224
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-11-10 00:31:56 +0800

    Small Update
    * Buffing "Resolve of Zefra" to remove the HOPT on the activation. Zefras are almost entirely Pendulum anyway, so it isn't always they reach the GY or banishment.

--- script/custom/c2002000084.lua
+++ script/custom/c2002000084.lua
@@ -1,5 +1,4 @@
 --Resolve of Zefra
-
 local s,id=GetID()
 function s.initial_effect(c)
 	local e0=aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsSetCard,SET_ZEFRA))
@@ -12,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	-- e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
