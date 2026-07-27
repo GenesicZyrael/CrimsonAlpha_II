e29392c8d7bb9fbce654ef43442ed82710dcee59
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2026-07-28 00:34:38 +0800

    Minor Rework
    * Elemental HERO's Resolve of Earth - can now only send the other materials from GY

--- script/custom/c2002000276.lua
+++ script/custom/c2002000276.lua
@@ -33,19 +33,35 @@ s.listed_names={alias}
 -- ==========================================
 -- Effect 2: Revive, Send & Extra Deck Lock
 -- ==========================================
-function s.edfilter(c)
+function s.edfilter(c,e,tp)
     if not c:IsType(TYPE_FUSION) then return false end
-    return c.material and c:ListsCodeAsMaterial(alias)
+    local mats=c.material
+    if not mats then return false end
+    local alias_is_mat=c:ListsCodeAsMaterial(alias)
+    local other_mats={}
+    for _,code in ipairs(mats) do
+        if code~=alias then
+            table.insert(other_mats,code)
+        end
+    end
+    if not alias_is_mat or #other_mats==0 then return false end
+    return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil,other_mats)
+    -- if not c:IsType(TYPE_FUSION) then return false end
+    -- return c.material and c:ListsCodeAsMaterial(alias)
 end
-function s.tgfilter(c)
-    return c:IsType(TYPE_MONSTER) and c:IsSetCard(SET_ELEMENTAL_HERO) 
-		and c:IsAbleToGrave() and not c:IsAttribute(ATTRIBUTE_EARTH)
+function s.tgfilter(c,other_mats)
+    return c:IsType(TYPE_MONSTER) and c:IsAbleToGrave() 
+        and c:IsCode(table.unpack(other_mats))
+    -- return c:IsType(TYPE_MONSTER) and c:IsSetCard(SET_ELEMENTAL_HERO) 
+		-- and c:IsAbleToGrave() and not c:IsAttribute(ATTRIBUTE_EARTH)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.edfilter,tp,LOCATION_EXTRA,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.edfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-    local g=Duel.SelectMatchingCard(tp,s.edfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,s.edfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
     Duel.ConfirmCards(1-tp,g)
+    -- Store the revealed Fusion Monster's ID to parse materials at resolution
+    e:SetLabel(g:GetFirst():GetCode())
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
@@ -68,17 +84,40 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     Duel.RegisterEffect(e1,tp)  
     -- Proceed with Summoning
     if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-        -- Optional Send to GY (Any 1 monster)
-        if Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) 
-            and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-            local sg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
-            if #sg>0 then
-                Duel.BreakEffect()
-                Duel.SendtoGrave(sg,REASON_EFFECT)
+        -- Retrieve the revealed Fusion Monster's ID
+        local code=e:GetLabel()
+        local tc=Duel.GetFirstMatchingCard(Card.IsCode,tp,LOCATION_EXTRA,0,nil,code)
+        if tc and tc.material then
+            -- Collect the explicitly listed materials that are not Sparkman
+            local other_mats={}
+            for _,mcode in ipairs(tc.material) do
+                if mcode~=alias then
+                    table.insert(other_mats,mcode)
+                end
+            end
+            -- Search for the listed material
+            if #other_mats>0 then
+                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+                local sg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil,other_mats)
+                if #sg>0 then
+					Duel.BreakEffect()
+					Duel.SendtoGrave(sg,REASON_EFFECT)
+                end
             end
         end
     end
+    -- if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+        -- -- Optional Send to GY (Any 1 monster)
+        -- if Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) 
+            -- and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+            -- Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+            -- local sg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+            -- if #sg>0 then
+                -- Duel.BreakEffect()
+                -- Duel.SendtoGrave(sg,REASON_EFFECT)
+            -- end
+        -- end
+    -- end
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
     return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION)
